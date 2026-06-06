b81c3a42fae34b4a871f2fc4ac186a505da9e535
S-Nrupendra <snrupendra@gmail.com>
2026-06-06 17:05:22 +0530

    Chat routed

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { getMessages, deleteMessage } = require('../controllers/message.controller');
+const { protect } = require('../middleware/auth.middleware');
+
+router.get('/', protect, getMessages);
+router.delete('/:id', protect, deleteMessage);
+
 module.exports = router;
